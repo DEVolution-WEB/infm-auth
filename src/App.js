0687d44e9efc438b6c18433e12b1a653363d8241
bensabri <ssabril@ymail.com>
2021-10-07 15:05:26 +0100

    changement texte

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
 				<p>DEVolution Team</p>
-				<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-					Branche de Sabri
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Branche de Sabri-dev build auth
 				</a>
 			</header>
 		</div>
