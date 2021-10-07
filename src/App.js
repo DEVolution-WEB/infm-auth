abab8f3feac5dd4f969e067b7a4654d280439147
amyre <benhammouda.amir@gmail.com>
2021-10-07 16:46:55 +0200

    PR

--- src/App.js
+++ src/App.js
@@ -3,17 +3,12 @@ import './App.css';
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
 				<p>DEVolution Team</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Branche de Sabri-dev build auth
+				<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+					Branche de Sabri-dev build auth Amir validate PR
 				</a>
 			</header>
 		</div>
