ffc3a01822d464224d000416c5c6f0ed591f2821
cestpasso <trashinscri@gmail.com>
2021-10-10 16:23:42 +0200

    navbar structure

--- src/components/Login.js
+++ src/components/Login.js
@@ -28,6 +28,16 @@ const Login = () => {
 
 	return (
 		<div className="h-screen flex bg-grey-light">
+			<nav>
+				<img src="" alt="logo INFM" />
+				<ul>
+				<li> Accueil </li>
+				<li> Accueil </li>
+				<li> Accueil </li>
+				<li> Accueil </li>
+				</ul>
+			</nav>
+
 			<div className="w-full max-w-md m-auto bg-white rounded-lg border border-primaryBorder shadow-default py-10 px-16">
 				<h1 className="text-2xl font-medium text-primary mt-4 mb-12 text-center">
 					Page de connection
