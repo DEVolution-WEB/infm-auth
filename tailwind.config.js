dc7470468adb5ec0205c793805ca14efa1ed5748
cestpasso <trashinscri@gmail.com>
2021-10-16 14:19:04 +0200

    breakpoint navxl responsive navbar/logo resize

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
 	purge: [],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		
 		extend: {
 			colors: {
 				darkBlueCust : '#312783',
@@ -12,8 +11,13 @@ module.exports = {
 				yellowCust : '#ffdd0e',
 				blueCust : '#7a73af',
 				lightGrayCust :'#8e88bb',
-				grayContentCust : '#f2f2f2'
+				grayContentCust : '#f2f2f2',
+				graySidebarHover : '#eae9f2'
 			},
+		screens: {
+			'navxl': '1300px',
+			
+		  },
 			
 		},
 	},
