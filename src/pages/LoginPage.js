b6375dc056fc5a3ce5d02b8a2968ee4980af2a8d
bensabri <ssabril@ymail.com>
2021-10-22 15:10:16 +0100

    login add textfiel mui

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,6 +2,8 @@ import React, { useRef, useState } from 'react';
 import { useAuth } from '../AuthContext';
 import { useHistory } from 'react-router-dom';
 import HeaderTitle from '../components/Layout/HeaderTitle';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
 import '../App.css';
 
 const LoginPage = () => {
@@ -21,7 +23,9 @@ const LoginPage = () => {
 			setLoading(true);
 			await login(emailRef.current.value, passwordRef.current.value);
 		} catch {
-			return setError('Votre e-mail ou votre mot de passe est incorrect.');
+			return setError(
+				'Votre e-mail ou votre mot de passe est incorrect.'
+			);
 		}
 		setLoading(false);
 		history.push('/dashboard');
@@ -29,25 +33,64 @@ const LoginPage = () => {
 
 	return (
 		<>
-			<HeaderTitle title='Espace Membre' />
-			<video className='z-n1 absolute w-screen h-screen object-cover' autoPlay loop muted>
-				<source src='https://firebasestorage.googleapis.com/v0/b/infm-773d8.appspot.com/o/infm-live.mp4?alt=media&token=05502d75-50e1-4ce0-aa05-7c07563806ba' type='video/mp4' />
+			<HeaderTitle title="Espace Membre" />
+			<video
+				className="z-n1 absolute w-screen h-screen object-cover"
+				autoPlay
+				loop
+				muted
+			>
+				<source
+					src="https://firebasestorage.googleapis.com/v0/b/infm-773d8.appspot.com/o/infm-live.mp4?alt=media&token=05502d75-50e1-4ce0-aa05-7c07563806ba"
+					type="video/mp4"
+				/>
 				Your browser does not support the video tag.
 			</video>
-			<div className='flex bg-transparent justify-center mt-20'>
-				<div className='w-86 h-64  mt-10 bg-darkBlueCust text-white flex justify-center rounded-xl shadow-md py-4 px-8'>
+			<div className="flex bg-transparent justify-center mt-20">
+				<div className="w-86 h-64  mt-10 bg-darkBlueCust text-white flex justify-center rounded-xl shadow-md py-4 px-8">
 					<form onSubmit={handleSubmit}>
-						<div>
-							<label htmlFor='email'>Adresse e-mail</label>
-							<input type='email' ref={emailRef} className='w-full p-2 text-primary text-black border outline-none text-sm transition duration-150 ease-in-out mt-2 mb-4' id='email' placeholder='Votre e-mail' required />
-						</div>
-						<div>
-							<label htmlFor='password'>Mot de passe</label>
-							<input type='password' ref={passwordRef} className='w-full p-2 text-primary text-black border outline-none text-sm transition duration-150 ease-in-out mt-2 mb-2' id='password' placeholder='Votre mot de passe' required />
-						</div>
-						{error && <p className='text-xs text-yellowCust'>{error}</p>}
-						<div className='flex flex-col justify-center items-center'>
-							<button disabled={loading} type='submit' className='mt-3 py-2 px-4 text-sm bg-yellowCust text-black rounded focus:outline-none hover:bg-yellow-400 transition duration-200'>
+						<Box noValidate>
+							<TextField
+								id="email"
+								label="Adresse e-mail"
+								type="email"
+								variant="outlined"
+								inputRef={emailRef}
+								required={true}
+								margin="normal"
+								style={{
+									backgroundColor: 'white',
+									width: '80%',
+									marginTop: '1rem',
+									borderRadius: '5px',
+								}}
+							/>
+							<TextField
+								// error
+								id="password"
+								label="Mot de passe"
+								type="password"
+								variant="outlined"
+								inputRef={passwordRef}
+								required={true}
+								style={{
+									backgroundColor: 'white',
+									width: '80%',
+									marginTop: '1rem',
+									marginBottom: '1rem',
+									borderRadius: '5px',
+								}}
+							/>
+						</Box>
+						{error && (
+							<p className="text-xs text-yellowCust">{error}</p>
+						)}
+						<div className="flex flex-col justify-center items-center">
+							<button
+								disabled={loading}
+								type="submit"
+								className="mt-3 py-2 px-4 text-sm bg-yellowCust text-black rounded focus:outline-none hover:bg-yellow-400 transition duration-200"
+							>
 								Se connecter
 							</button>
 						</div>
