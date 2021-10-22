d6a906ccc98d244eb319850f25dd54c79cd52be5
bensabri <ssabril@ymail.com>
2021-10-22 14:38:24 +0100

    delete the value formation_id from update component

--- src/components/Formation/FormationBlock.js
+++ src/components/Formation/FormationBlock.js
@@ -17,7 +17,6 @@ const FormationBlock = () => {
 		//   UPDATE FORMATIONS FROM DATABASE
 		await updateDoc(doc(db, 'formations', id), {
 			formation_name: nameFormation,
-			formation_id: formationId,
 			video_url: url,
 		})
 			.then(() => alert('Modifier avec success'))
@@ -30,50 +29,92 @@ const FormationBlock = () => {
 	};
 
 	return (
-		<div className='mb-10 p-12 shadow-md rounded'>
+		<div className="mb-10 p-12 shadow-md rounded">
 			<div>
-				<h2 className='text-2xl px-5 font-light text-primary mt-0 mb-8'>Modifier une formation</h2>
-				<div className='inline mb-2 space-x-36'>
-					<div className='w-64 max-w-4xl'>
-						<label htmlFor='user'>Liste des Formations:</label>
-						<select className='block w-full bg-gray-100 text-black opacity-80 border-b border-white py-3 px-4 mb-3 mt-1 leading-tight focus:outline-none focus:border-yellowCust' name='user' onChange={(e) => setQuery(e.target.value)}>
+				<h2 className="text-2xl px-5 font-light text-primary mt-0 mb-8">
+					Modifier une formation
+				</h2>
+				<div className="inline mb-2 space-x-36">
+					<div className="w-64 max-w-4xl">
+						<label htmlFor="user">Liste des Formations:</label>
+						<select
+							className="block w-full bg-gray-100 text-black opacity-80 border-b border-white py-3 px-4 mb-3 mt-1 leading-tight focus:outline-none focus:border-yellowCust"
+							name="user"
+							onChange={(e) => setQuery(e.target.value)}
+						>
 							<option></option>
-							{formations.sort().map(({ id, data: { formation_name } }) => (
-								<option key={id} value={formation_name}>
-									{formation_name}
-								</option>
-							))}
+							{formations
+								.sort()
+								.map(({ id, data: { formation_name } }) => (
+									<option key={id} value={formation_name}>
+										{formation_name}
+									</option>
+								))}
 						</select>
 					</div>
 				</div>
 
 				{formations
-					.filter(({ data: { formation_name } }) => formation_name === query)
+					.filter(
+						({ data: { formation_name } }) =>
+							formation_name === query
+					)
 					.map(({ id, data: { formation_name, video_url } }) => (
 						<div key={id}>
-							<ModalForma id={id} handleDeleteFormations={handleDeleteFormations} />
-							<div className='w-64 bg-white py-5 px-3'>
+							<ModalForma
+								id={id}
+								handleDeleteFormations={handleDeleteFormations}
+							/>
+							<div className="w-64 bg-white py-5 px-3">
 								<div>
-									<div className='flex mb-5 space-x-2'>
-										<h1 className='text-xl text-black'>Formation:</h1>
-										<p className='text-md text-black '>{formation_name}</p>
+									<div className="flex mb-5 space-x-2">
+										<h1 className="text-xl text-black">
+											Formation:
+										</h1>
+										<p className="text-md text-black ">
+											{formation_name}
+										</p>
 									</div>
-									<div className='flex mb-3 space-x-2'>
-										<h1 className='text-xl text-black'>URL:</h1>
-										<p className='text-md text-black overflow-x-scroll'>{video_url}</p>
+									<div className="flex mb-3 space-x-2">
+										<h1 className="text-xl text-black">
+											URL:
+										</h1>
+										<p className="text-md text-black overflow-x-scroll">
+											{video_url}
+										</p>
 									</div>
-									<div class='flex justify-center'>
-										<button onClick={() => setEdit(!edit)} className='bg-darkBlueCust mt-3 mr-2 py-2 px-4 text-sm text-white rounded border border-blue-light focus:outline-none focus:border-primary hover:bg-blue-800 transition hover:duration-300'>
+									<div class="flex justify-center">
+										<button
+											onClick={() => setEdit(!edit)}
+											className="bg-darkBlueCust mt-3 mr-2 py-2 px-4 text-sm text-white rounded border border-blue-light focus:outline-none focus:border-primary hover:bg-blue-800 transition hover:duration-300"
+										>
 											Editer
 										</button>
-										<button onClick={() => setModalForma(!modalForma)} className='bg-darkBlueCust mt-3 py-2 px-4 text-sm text-white rounded border border-blue-light focus:outline-none focus:border-primary hover:bg-blue-800 transition hover:duration-300'>
+										<button
+											onClick={() =>
+												setModalForma(!modalForma)
+											}
+											className="bg-darkBlueCust mt-3 py-2 px-4 text-sm text-white rounded border border-blue-light focus:outline-none focus:border-primary hover:bg-blue-800 transition hover:duration-300"
+										>
 											Supprimer
 										</button>
 									</div>
 								</div>
 							</div>
-							<div className='bg-white' key={id}>
-								{edit && <Editformation edit={edit} setEdit={setEdit} handleEditFormations={handleEditFormations} id={id} setNameFormation={setNameFormation} setUrl={setUrl} setFormationId={setFormationId} />}
+							<div className="bg-white" key={id}>
+								{edit && (
+									<Editformation
+										edit={edit}
+										setEdit={setEdit}
+										handleEditFormations={
+											handleEditFormations
+										}
+										id={id}
+										setNameFormation={setNameFormation}
+										setUrl={setUrl}
+										setFormationId={setFormationId}
+									/>
+								)}
 							</div>
 						</div>
 					))}
