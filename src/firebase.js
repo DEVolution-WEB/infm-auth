43caba9e2524b63c25f6dc1e56447566d80dc254
amyre <benhammouda.amir@gmail.com>
2021-10-23 16:54:44 +0200

    Triyng to add Analytics - Cleaning firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,20 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
+import 'firebase/compat/analytics';
 
-const app = firebase.initializeApp({
-	apiKey: "AIzaSyCpEvpxo8VHfs81oqDkLdGpKePR7niLF_o",
-  	authDomain: "infm-773d8.firebaseapp.com",
-  	projectId: "infm-773d8",
-  	storageBucket: "infm-773d8.appspot.com",
-  	messagingSenderId: "36573213521",
-  	appId: "1:36573213521:web:a71cd5ab57d6a0e12cd970",
-  	measurementId: "G-K9X8D7GMEX"
+firebase.initializeApp({
+	apiKey: 'AIzaSyCpEvpxo8VHfs81oqDkLdGpKePR7niLF_o',
+	authDomain: 'infm-773d8.firebaseapp.com',
+	projectId: 'infm-773d8',
+	storageBucket: 'infm-773d8.appspot.com',
+	messagingSenderId: '36573213521',
+	appId: '1:36573213521:web:a71cd5ab57d6a0e12cd970',
+	measurementId: 'G-K9X8D7GMEX',
 });
 
-const googleProvider = new firebase.auth.GoogleAuthProvider();
-const facebookProvider = new firebase.auth.FacebookAuthProvider();
-const db = app.firestore();
+firebase.analytics();
+const auth = firebase.auth();
+const db = firebase.firestore();
 
-export const auth = app.auth();
-export { app, googleProvider, facebookProvider, db };
+export { auth, db };
