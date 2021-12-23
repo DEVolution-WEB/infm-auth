c27b0895676fad9e475648d31a7160dc7539ffe3
amyre <benhammouda.amir@gmail.com>
2021-12-23 11:34:48 +0100

    cloning project from infm

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/analytics';
 
 firebase.initializeApp({
-	apiKey: 'AIzaSyCpEvpxo8VHfs81oqDkLdGpKePR7niLF_o',
-	authDomain: 'infm-773d8.firebaseapp.com',
-	projectId: 'infm-773d8',
-	storageBucket: 'infm-773d8.appspot.com',
-	messagingSenderId: '36573213521',
-	appId: '1:36573213521:web:a71cd5ab57d6a0e12cd970',
-	measurementId: 'G-K9X8D7GMEX',
+	apiKey: 'AIzaSyCv2XH5_SY3BXNrRj8cNkSRNvpYB-4Su_E',
+	authDomain: 'kelly-modules.firebaseapp.com',
+	projectId: 'kelly-modules',
+	storageBucket: 'kelly-modules.appspot.com',
+	messagingSenderId: '960680725795',
+	appId: '1:960680725795:web:238805b1b2738a3e301096',
+	measurementId: 'G-4DJ5EH5CFP',
 });
 
 firebase.analytics();
